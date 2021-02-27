066d585607bddc45721706f36c495de9989e32fa
Billy Le <lebilly87@gmail.com>
2021-02-27 07:06:17 -0800

    updated db

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -8,7 +8,11 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        uri: process.env.DATABASE_URL,
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
         ssl: {
           rejectUnauthorized: false,
         },
