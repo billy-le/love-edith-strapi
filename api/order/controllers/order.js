270a44eb7b5f99b150b23d2973f4de0fcb58cf4a
Billy Le <lebilly87@gmail.com>
2021-04-09 21:45:55 -0700

    updated email template

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -130,10 +130,7 @@ module.exports = {
 
     const dueDate = dateFns.addHours(new Date(created_at), 48);
 
-    const payment =
-      payment_method === "bpi"
-        ? "BPI Account Number: 1519079875"
-        : "GCASH: 09162889221";
+    const payment = payment_method === "bpi" ? "BPI" : "GCASH";
 
     const shipping_method =
       shipping == "0"
@@ -142,20 +139,21 @@ module.exports = {
         ? "Metro Manila"
         : "Outside Metro Manila";
 
-    const htmlTemplate = `<!DOCTYPE html><html lang="en" style="box-sizing:border-box"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Love Edith</title></head><body style="font-family:'Segoe UI',Tahoma,Geneva,Verdana,sans-serif;border-collapse:collapse"><table cellspacing="0" cellpadding="0" border="0" bgcolor="#fff" style="width:100%"><thead><tr><th><h2 style="margin-bottom:24px">Hi, love! &#9825; We've received your order.</h2></th></tr></thead><tbody><tr><td colspan="4"><h4 style="margin:0;text-decoration-line:underline">Issued To:</h4><p style="margin:0">${first_name} ${last_name}</p><p style="margin:0">${email}</p><p style="margin:0">+63 ${contact_number}</p></td></tr><tr style="height:24px"></tr><tr><td><h4 style="margin:0;text-decoration-line:underline">Shipment Details:</h4><p style="margin:0">${house_building_unit} ${street}</p><p style="margin:0">Barangay ${barangay}, ${city}</p><p style="margin:0">${province} ${region}</p><p style="margin:0">${landmarks}</p></td></tr><tr style="height:24px"></tr><tr><td><h4 style="margin:0;text-decoration-line:underline">Shipping Method:</h4><p style="margin:0">${shipping_method}</p></td></tr><tr style="height:24px"></tr><tr style="height:24px"></tr><tr><td><table style="max-width:400px;text-align:left;border-collapse:collapse;width:100%"><thead><tr><th><h4 style="margin:0;text-decoration-line:underline">Items</h4></th></tr></thead><tbody>${products
+    const htmlTemplate = `<!DOCTYPE html><html lang="en" style="box-sizing:border-box"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Love Edith</title></head><body style="font-family:'Segoe UI',Tahoma,Geneva,Verdana,sans-serif;border-collapse:collapse"><table cellspacing="0" cellpadding="0" border="0" bgcolor="#fff" style="width:100%"><thead><tr><th><h2 style="margin-bottom:24px">Hi, love! &#9825; We've received your order.</h2></th></tr></thead><tbody><tr><td colspan="4"><h4 style="margin:0;text-decoration-line:underline">Issued To:</h4><p style="margin:0">${first_name} ${last_name}</p><p style="margin:0">${email}</p><p style="margin:0">+63 ${contact_number}</p></td></tr><tr style="height:24px"></tr><tr><td><h4 style="margin:0;text-decoration-line:underline">Shipment Details:</h4><p style="margin:0">${house_building_unit} ${street}</p><p style="margin:0">Barangay ${barangay}, ${city}</p><p style="margin:0">${province} ${region}</p><p style="margin:0">${landmarks}</p></td></tr><tr style="height:24px"></tr><tr><td><h4 style="margin:0;text-decoration-line:underline">Shipping Method:</h4><p style="margin:0">${shipping_method}</p></td></tr><tr style="height:24px"></tr><tr style="height:24px"></tr><tr><td><table style="max-width:400px;text-align:left;border-collapse:collapse;width:100%"><thead><tr><th><h4 style="margin:0;text-decoration-line:underline">Item</h4></th><th>Qty</th><th>Price</th><th>Total</th></tr></thead><tbody>${products
       .map((item) => {
         const total = math.chain(item.qty).multiply(item.price).done();
-        return `<tr><td>${
-          item.name
-        }</td><td><span style="text-transform:capitalize">${
+        return `<tr><td>
+        <div>${item.name}</div>
+        <div><span style="text-transform: uppercase;">${
           item.color
-        }</span> | <span style="text-transform:capitalize">${
+        }</span> | <span style="text-transform: uppercase;">${
           item.size
-        }</span></td><td></td></tr><tr><td>${item.qty}x</td><td>${PHP(
-          item.price
-        ).format()}</td><td>${PHP(
-          total
-        ).format()}</td></tr><tr style="height: 8px"></tr>`;
+        }</span></div>
+        </td>
+        <td>${item.qty}</td>
+        <td>${PHP(item.price).format()}</td>
+        <td>${PHP(total).format()}</td>
+        </tr><tr style="height: 8px"></tr>`;
       })
       .join("")}<tr style="height:24px"></tr><tr><td><td>Subtotal</td><td>${PHP(
       sub_total
@@ -167,9 +165,11 @@ module.exports = {
             discount
           ).format()}</td></tr>`
         : ""
-    }<tr><td><td>Total</td><td>${PHP(total).format()}</td></tr>${
+    }<tr style="border-top: 1px solid black;"><td><td style="font-weight: 500;">Total</td><td style="font-weight: 500;">${PHP(
+      total
+    ).format()}</td></tr>${
       hasItemsRemoved
-        ? `<tr style="margin-top: 8px"><td colspan="3" style="font-size: 10px">* Some items have been removed due to availability</td></tr>`
+        ? `<tr><td colspan="3" style="font-size: 10px; padding-top: 8px;">* Some items have been removed due to availability</td></tr>`
         : ""
     }</tbody></table></td></tr><tr style="height:24px"></tr><tr><td><h4 style="margin:0;text-decoration-line:underline">Payment Method:</h4><p style="margin:0;">${payment}</p></td></tr><tr style="height:24px"></tr><tr><td><h4 style="margin:0;text-decoration-line:underline">Payment Due Date:</h4><p style="margin:0">${dateFns.format(
       dueDate,
